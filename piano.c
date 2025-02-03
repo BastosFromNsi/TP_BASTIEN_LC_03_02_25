90e88b80bd496bbc48de427e4a09b6fc588f0052
Bastien Le Corguille <bastien.bruges@icloud.com>
2025-02-03 19:42:45 +0100

    exercice 3,4,5 fini et rectifications

--- piano.c
+++ piano.c
@@ -16,7 +16,7 @@ void libererNote(Note *note)
     note = NULL;
 }
 
-void SimulerNote(Note **note, int nb_notes)
+void SimulerPiano(Note **note, int nb_notes)
 {
 
     if (nb_notes > 12)
@@ -39,6 +39,11 @@ void SimulerNote(Note **note, int nb_notes)
                 {
                     for (int i = 0; i < nb_notes; i++)
                     {
+                        if (n == 'a' + i)
+                        {
+                            Beep(note[i]->frequence, 500);
+                            break;
+                        }
                     }
                 }
             }
@@ -49,7 +54,23 @@ void SimulerNote(Note **note, int nb_notes)
 int main()
 {
     int nb_note = 12;
+    Note *notes[12];
 
     float frequencies[12] = {261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99,
                              392.00, 415.30, 440.00, 466.16, 493.88};
+
+    for (int i = 0; i < nb_note; i++)
+    {
+        notes[i] = creerNote();
+        notes[i]->frequence = frequencies[i];
+    }
+
+    SimulerPiano(notes, nb_note);
+
+    for (int i = 0; i < nb_note; i++)
+    {
+        libererNote(notes[i]);
+    }
+
+    return 0;
 }
